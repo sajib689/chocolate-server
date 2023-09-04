1f33d20031dcd8916eb93550a6d432924acfcf93
sajib689 <jibonraja11@gmail.com>
2023-09-04 19:06:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+const port = process.env.PORT || 3000
+ app.use(cors())
+ app.use(express.json())
+
+
+ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2m0rny5.mongodb.net/?retryWrites=true&w=majority`;
+ console.log(uri)
+ // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+ const client = new MongoClient(uri, {
+   serverApi: {
+     version: ServerApiVersion.v1,
+     strict: true,
+     deprecationErrors: true,
+   }
+ });
+ 
+ async function run() {
+   try {
+     // Connect the client to the server	(optional starting in v4.7)
+     await client.connect();
+     const chocolatesCollection = client.db('chocolateDb').collection('chocolates');
+     app.post('/chocolates', async(req, res) => {
+        const query = req.body
+        console.log(query)
+        const chocolate = await chocolatesCollection.insertOne(query)
+        res.send(chocolate)
+     })
+    //  get data from database
+    app.get('/chocolates', async(req, res) => {
+        const result = await chocolatesCollection.find().toArray();
+        res.send(result)
+    })
+    // delete data from database
+    app.delete('/chocolates/:id', async(req,res) => {
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const result = await chocolatesCollection.deleteOne(query)
+        res.send(result)
+    });
+    // calls single data from data base
+    app.get('/chocolates/:id', async(req, res) =>{
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const result = await chocolatesCollection.findOne(query)
+        res.send(result)
+    })
+    // update data from client to database
+    app.put('/chocolates/:id', async(req, res) => {
+        const id = req.params.id
+        const updateChocolate = req.body
+        const query = {_id: new ObjectId(id)}
+        const options = {upsert: true}
+        const updatedChocolate = {
+            $set: {
+                name: updateChocolate.name, 
+            country: updateChocolate.country,
+            image: updateChocolate.image, 
+            category: updateChocolate.category
+            }
+        }
+        const result = await chocolatesCollection.updateOne( query,updatedChocolate, options)
+        res.send(result)
+    })
+     // Send a ping to confirm a successful connection
+     await client.db("admin").command({ ping: 1 });
+     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   } finally {
+     // Ensures that the client will close when you finish/error
+    //  await client.close();
+   }
+ }
+ run().catch(console.dir);
+ 
+
+
+ app.get('/', (req, res) => {
+    res.send('Welcome the server is running')
+ })
+
+ app.listen(port, () => {
+    console.log(`Listening on ${port}`)
+ })
